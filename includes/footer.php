74bc56699e980ac03ac07ae0c1a6c5d05e02d50f
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-21 13:32:19 -0600

    Committing experimental files.

--- includes/footer.php
+++ includes/footer.php
@@ -13,15 +13,7 @@
     $(document).ready(function() {
 
       $("#owl-demo").owlCarousel({
-
-          slideSpeed: 800,
-          paginationSpeed: 800,
-          singleItem: true,
-          autoPlay: 7000,
-          navigation: true,
-          navigationText: ["<i class='fa fa-arrow-circle-left'></i>", "<i class='fa fa-arrow-circle-right'></i>"],
-          stopOnHover: true,
-
+            loop: true
       });
 
     });
