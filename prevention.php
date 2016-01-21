0d84850914c827a406cf997df06be2df2b5b3cfd
TyMerrill767 <tyler_merrill2011@hotmail.com>
2016-01-21 09:44:12 -0600

    Updating headers, adding Brian's content.

--- prevention.php
+++ prevention.php
@@ -11,9 +11,10 @@
    <main role="main">
         <h1>Prevention and Precautions</h1>
         <p>You know the dangers. How do you prevent tragedy?</p>
-        <h3>INSIDE THE VEHICLE</h3>
 
-        <h4>Heat Stroke/Hypothermia:</h4>
+        <h2>Inside the Vehicle</h2>
+
+        <h3>Heat Stroke/Hypothermia:</h3>
         <p>Position your child&rsquo;s car seat on the passenger side to see your child easier.</p>
         <p>Stage your car with cues to help remember your child:</p>
         <ul>
@@ -23,7 +24,7 @@
         </ul>
         <p>Many devices and apps are now available to help you remember your child.</p>
 
-        <h4>Choking:</h4>
+        <h3>Choking:</h3>
         <p>Clean your vehicle regularly &ndash; especially of small objects such as trinkets, toys, or stray food items that may have fallen into hidden spaces; be sure to look under seats and in storage areas.</p>
         <p>You should never leave a small child unattended while they are eating.</p>
         <p>Children should never eat in the car.</p>
@@ -31,36 +32,31 @@
         <p>Feed your child at home before or after your errands.</p>
         <p>Keep your vehicle clean and free of loose small items.</p>
 
-        <h4>Front-overs/Back-overs:</h4>
+        <h3>Front-overs/Back-overs:</h3>
         <p>Average blindzone: 15-25 feet &lpar;shorter drivers have a greater blindzone&rpar;</p>
         <p>Teach your children that any vehicle can move</p>
         <p>Keep toys out of the driveway</p>
         <p>Know where your children are before moving a vehicle</p>
 
-        <h4>Vehicle accidentally set in motion:</h4>
+        <h3>Vehicle accidentally set in motion:</h3>
         <p>Shut the vehicle off and lock it when you leave it</p>
         <p>Keep your car locked when not in use</p>
         <p>Keep keys out of reach of children</p>
 
-        <h4>Power Windows:</h4>
+        <h3>Power Windows:</h3>
         <p>When children are riding, engage the child safety lock</p>
         <p>Shut the vehicle off and lock it when you leave it</p>
         <p>Teach children that power windows are dangerous</p>
 
-        <h4>Trunk Entrapment</h4>
+        <h2>Outside the Vehicle</h2>
+        <p>Always bring your child with you when you exit the vehicle – no amount of time is safe to leave your child unattended in the vehicle, even for “just a minute”, and it is illegal to do so in some states</p>
+        <p>Be sure that you lock the vehicle’s doors and trunk after exiting with your child – even when parked and at home, a vehicle can endanger a child, as they can become trapped within it if left open</p>
+        <p>Instruct children that the vehicle is not a safe place to play</p>
 
-        <h4>Abduction</h4>
-
-        <h4>Poison</h4>
-
-        <h3>OUTSIDE THE VEHICLE</h3>
-        <p>Always bring your child with you when you exit the vehicle. No amount of time is safe to leave your child unattended, even for &ldquo;just a minute&rdquo;. It is illegal to do so in most states.</p>
-        <p>Be sure that your vehicle&rsquo;s doors and trunk remain closed and locked, even when parked at home. Children love to play hide-and-seek and other fun games. A vehicle may seem to them an ideal hiding spot, a stagecoach, a castle, or many other things. A vehicle can quickly become dangerous, as the child can easily become trapped within it. Instruct children that the vehicle is not a safe place to play.</p>
-        <p>Familiarize yourself with life-saving techniques such as the Heimlich Maneuver, child cardiopulmonary resuscitation &lpar;CPR&rpar;, and other first-aid measures, and call 911 for emergencies.</p>
-        <p>Make caretakers aware of the dangers and solutions, and always have a plan of action in case of emergency.</p>
-        <p>Greatest of all: YOU KNOW YOUR CHILD. If they are exhibiting abnormal behavior or exhibiting any of the signs of illness or injury, call the doctor or emergency services immediately!</p>
-
-       </section>
+        <h2>In General</h2>
+        <p>Familiarize yourself with life-saving techniques such as the Heimlich Maneuver, child cardiopulminary resuscitation (CPR), and other first-aid measures, and call 911 for emergencies.</p>
+        <p>Be sure your child's caretaker(s) are aware of the dangers and solutions, and always have a plan of action in case of emergency.</p>
+        <p>Be aware of your child's behavior and condition: if they are acting strange or confused, or if they are exhibiting nausea, dizziness, sudden cessation of sweating, or quick/slow pulse, they may be suffering heat stroke.</p>
 
    <a href="action.php" class="next-page-link">Find out how you can take action &gt;&gt;</a>
    </main>
