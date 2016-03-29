0e2d5f595fe3190f2e61501cd1e6176a6acdc3d6
Tyler Merrill <tyler_merrill2011@hotmail.com>
2016-03-29 00:43:09 -0500

    Remove BPA Information section as well as Matt's information section.

--- about.php
+++ about.php
@@ -10,13 +10,6 @@
    <main role="main">
         <h1>About Us</h1>
 
-        <h2>BPA Information:</h2>
-        <p>Chapter: 03-0042</p>
-        <p>Theme: Toddler Neglect in Vehicles</p>
-        <p>School: Francis Tuttle Technology Center</p>
-        <p>City: Oklahoma City, OK</p>
-        <p>Year: 2015</p><br/>
-
         <h2>The Team:</h2>
            <div class="cir_shape">
                 <img src="images/tylerV3.png" alt="Tyler Picture" class="tyler about_image">
@@ -31,22 +24,6 @@
                 <div class="profile">
                 <h3>Brian Richardson</h3>
                 <p>Brian Richardson is a Web Design and Development student with a background in art and graphic design. He focuses his creative experience toward becoming a developer/designer hybrid. After attending Oklahoma State University as a Studio Art major, Brian moved to Oklahoma&rsquo;s capital city and has worked in advertising, marketing, and printing. Brian is husband to a wife who is also a graphic designer and father to 2 wonderful children. In his free time, Brian likes to spend time in the great outdoors running, hiking, and camping.</p>
-                </div>
-           </div>
-
-           <div class="cir_shape">
-                <img src="images/abigailV3.png" alt="Abigail Picture" class="abigail about_image">
-                <div class="profile">
-                <h3>Abigail Duran</h3>
-                <p>Abigail is currently taking the Web Design and Development course at Francis Tuttle. She began the program in January 2015 and will complete in the Fall of 2016. </p>
-                </div>
-           </div>
-
-           <div class="cir_shape">
-                <img src="images/mattV3.png" alt="Matthew Picture" class="matthew about_image">
-                <div class="profile">
-                <h3>Matthew Sterling</h3>
-                <p>Matthew Sterling is currently a student at Francis Tuttle studying to become a Web Developer. Web development is something he has wanted to do since he was about 14. Matthew has a background in set building from his 4 years of taking stagecraft in high school. He likes to express his creativity through his music from which he plays 5 different instruments ranging from the Ocarina to the Guitar. Once he completes his classes for Web Development Matthew hopes to go back to school to get certified in audio production/development. Matthew likes longboarding, working out, and spending time with friends and family.</p>
                 </div>
            </div>
 
