d287f7d027fa2a0e12a85c71273553d7c6b181e5
Tyler Merrill <tyler_merrill2011@hotmail.com>
2016-03-29 00:44:26 -0500

    Adding BPA information to footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,13 @@
+<div id="bpa-info">
+    <hr>
+    <p>BPA Information:</p>
+    <p>Chapter: 03-0042</p>
+    <p>Theme: Toddler Neglect in Vehicles</p>
+    <p>School: Francis Tuttle Technology Center</p>
+    <p>City: Oklahoma City, OK</p>
+    <p>Year: 2016</p>
+</div>
+
 <script src="js/jquery.min.js"></script>
 <script src="js/jquery.slicknav.min.js"></script>
 <script src="js/owl.carousel.js"></script>
